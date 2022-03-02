fd870479ff4feeff93fc095a0fb8b36b92bffed7
yagoal <br.yago@gmail.com>
2022-03-02 19:08:27 -0300

    Code reuse tweakes and and tie lógica
    Implementado o reuso de código com tags e a lógica de quando o jogo da empate

--- JogoDaVelha/HomeViewController.swift
+++ JogoDaVelha/HomeViewController.swift
@@ -12,12 +12,10 @@ class HomeViewController: UIViewController {
 
     var model: Model = Model()
 
-
     lazy var stackViewLinha: UIStackView = {
         let stackView = UIStackView(frame: .zero)
         stackView.axis = .vertical
         stackView.spacing = 24.0
-       
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -36,12 +34,12 @@ class HomeViewController: UIViewController {
         stackViewLinha.addArrangedSubview(model.stackViewColuna2)
         stackViewLinha.addArrangedSubview(model.stackViewColuna3)
         stackViewLinha.addArrangedSubview(model.labelGanhador)
+        stackViewLinha.addArrangedSubview(model.buttonPlayAgain)
     }
 //        model.addSubViewsModel()
 
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            
             stackViewLinha.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             stackViewLinha.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0),
             stackViewLinha.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0),
--- JogoDaVelha/Model/Model.swift
+++ JogoDaVelha/Model/Model.swift
@@ -9,48 +9,17 @@ import UIKit
 
 
 class Model:UIViewController {
+
+    // MARK: - ATRIBUTOS
     var vencedor:Bool?
     var cont:Int = 0
     var matrizJogo:[[String]] = [["", "", ""], ["", "", ""], ["", "", ""]]
-
-    func rename () {
-        if cont % 2 == 0 {
-            labelGanhador.text = "Player 2 Wins!"
-        }
-        labelGanhador.isHidden = false
-    }
-
-    func ganhou () -> Bool{
-        for i in 0...2 {
-            if (matrizJogo[i][0] == matrizJogo[i][1]) && (matrizJogo[i][0] == matrizJogo[i][2]) && (matrizJogo[i][0] != "") {
-
-                rename()
-                return true
-            }
-          }
-        for i in 0...2 {
-            if (matrizJogo[0][i] == matrizJogo[1][i]) && (matrizJogo[0][i] == matrizJogo[2][i]) && (matrizJogo[0][i] != "") {
-                rename()
-                return true
-
-            }
-          }
-          if (matrizJogo[0][0] == matrizJogo[1][1]) && (matrizJogo[0][0] == matrizJogo[2][2]) && (matrizJogo[0][0] != "")  {
-              rename()
-              return true
-          }
-          if (matrizJogo[0][2] == matrizJogo[1][1]) && (matrizJogo[0][2] == matrizJogo[2][0]) && (matrizJogo[0][2] != "")  {
-              rename()
-              return true
-          }
-          return false
-    }
+    var matrizReset:[[String]] = [["", "", ""], ["", "", ""], ["", "", ""]]
 
     lazy var labelGanhador: UILabel = {
         let label = UILabel()
-        label.text = "Player 1 Wins!"
+        label.text = "Player 'X' Turn"
         label.font = UIFont (name: "Noteworthy-Bold", size: 45)
-        label.isHidden = true
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -92,207 +61,20 @@ class Model:UIViewController {
         return stackView
     }()
 
-
-    func addSubViewsModel () {
-        stackViewLinha.addArrangedSubview(stackViewColuna)
-        stackViewLinha.addArrangedSubview(stackViewColuna2)
-        stackViewLinha.addArrangedSubview(stackViewColuna3)
-        configureConstraintsButton()
-    }
-
-
-    @objc func jogada() {
-
-        if  cont % 2 == 0 {
-            button.setTitle("X", for: .normal)
-            button.isEnabled = false
-            matrizJogo[0][0] = "X"
-
-
-        } else {
-            button.setTitle("O", for: .normal)
-            button.isEnabled = false
-            matrizJogo[0][0] = "O"
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou() : false
-
-        print (vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-
-
-    @objc func jogada4() {
-
-        if cont % 2 == 0 {
-            button4.setTitle("X", for: .normal)
-            button4.isEnabled = false
-            matrizJogo[1][0] = "X"
-//            cont += 1
-
-        } else {
-            button4.setTitle("O", for: .normal)
-            button4.isEnabled = false
-            matrizJogo[1][0] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada2() {
-        if cont % 2 == 0 {
-            button2.setTitle("X", for: .normal)
-            button2.isEnabled = false
-            matrizJogo[0][1] = "X"
-//            cont += 1
-
-        } else {
-            button2.setTitle("O", for: .normal)
-            button2.isEnabled = false
-            matrizJogo[0][1] = "O"
-//            cont += 1
-        }
-        cont += 1
-
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada3() {
-        if cont % 2 == 0 {
-            button3.setTitle("X", for: .normal)
-            button3.isEnabled = false
-            matrizJogo[0][2] = "X"
-//            cont += 1
-
-        } else {
-            button3.setTitle("O", for: .normal)
-            button3.isEnabled = false
-            matrizJogo[0][2] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada5() {
-
-        if cont % 2 == 0 {
-            button5.setTitle("X", for: .normal)
-            button5.isEnabled = false
-            matrizJogo[1][1] = "X"
-//            cont += 1
-
-        } else {
-            button5.setTitle("O", for: .normal)
-            button5.isEnabled = false
-            matrizJogo[1][1] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada6() {
-
-        if cont % 2 == 0 {
-            button6.setTitle("X", for: .normal)
-            button6.isEnabled = false
-            matrizJogo[1][2] = "X"
-//            cont += 1
-
-        } else {
-            button6.setTitle("O", for: .normal)
-            button6.isEnabled = false
-            matrizJogo[1][2] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada7() {
-        if cont % 2 == 0 {
-            button7.setTitle("X", for: .normal)
-            button7.isEnabled = false
-            matrizJogo[2][0] = "X"
-//            cont += 1
-
-        } else {
-            button7.setTitle("O", for: .normal)
-            button7.isEnabled = false
-            matrizJogo[2][0] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada8() {
-
-        if cont % 2 == 0 {
-            button8.setTitle("X", for: .normal)
-            button8.isEnabled = false
-            matrizJogo[2][1] = "X"
-//            cont += 1
-
-        } else {
-            button8.setTitle("O", for: .normal)
-            button8.isEnabled = false
-            matrizJogo[2][1] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
-    @objc func jogada9() {
-       
-        if cont % 2 == 0 {
-            button9.setTitle("X", for: .normal)
-            button9.isEnabled = false
-            matrizJogo[2][2] = "X"
-//            cont += 1
-
-        } else {
-            button9.setTitle("O", for: .normal)
-            button9.isEnabled = false
-            matrizJogo[2][2] = "O"
-//            cont += 1
-        }
-        cont += 1
-        vencedor = cont > 4 ? ganhou(): false
-        print(vencedor!)
-        print (matrizJogo)
-        print (cont)
-    }
-
+    lazy var buttonPlayAgain: UIButton = {
+        let button = UIButton ()
+        button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 50)
+        button.setTitle("Restart", for: .normal)
+        button.layer.cornerRadius = 30
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(playAgain), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     lazy var button: UIButton = {
         let button = UIButton()
+        button.tag = 00
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
@@ -303,101 +85,203 @@ class Model:UIViewController {
 
     lazy var button2: UIButton = {
         let button = UIButton()
+        button.tag = 01
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
-        button.addTarget(self, action: #selector(jogada2), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button3: UIButton = {
         let button = UIButton()
+        button.tag = 02
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
 
-        button.addTarget(self, action: #selector(jogada3), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button4: UIButton = {
         let button = UIButton()
+        button.tag = 10
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
 
-        button.addTarget(self, action: #selector(jogada4), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button5: UIButton = {
         let button = UIButton()
+        button.tag = 11
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
-        button.addTarget(self, action: #selector(jogada5), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button6: UIButton = {
         let button = UIButton()
+        button.tag = 12
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
 
-        button.addTarget(self, action: #selector(jogada6), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button7: UIButton = {
         let button = UIButton()
+        button.tag = 20
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
 
-        button.addTarget(self, action: #selector(jogada7), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button8: UIButton = {
         let button = UIButton()
+        button.tag = 21
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
 
-        button.addTarget(self, action: #selector(jogada8), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     lazy var button9: UIButton = {
         let button = UIButton()
+        button.tag = 22
         button.titleLabel?.font = UIFont(name: "Noteworthy-Bold", size: 80)
         button.setTitle("", for: .normal)
         button.backgroundColor = .systemBlue
-
-        button.addTarget(self, action: #selector(jogada9), for: .touchUpInside)
+        button.addTarget(self, action: #selector(jogada), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
+    var allButtons: [UIButton] {
+        [button, button2, button3, button4, button5, button6, button7, button8, button9]
+    }
+
+
+    // MARK: - FUNCTIONS
+
+    @objc func jogada(_ sender: UIButton) {
+        if  cont % 2 == 0 {
+            sender.setTitle("X", for: .normal)
+            sender.isEnabled = false
+
+        } else {
+            sender.setTitle("O", for: .normal)
+            sender.isEnabled = false
+
+        }
+        switch sender.tag {
+            case 00: matrizJogo[0][0] = cont % 2 == 0 ? "X" : "O"
+            case 01: matrizJogo[0][1] = cont % 2 == 0 ? "X" : "O"
+            case 02: matrizJogo[0][2] = cont % 2 == 0 ? "X" : "O"
+            case 10: matrizJogo[1][0] = cont % 2 == 0 ? "X" : "O"
+            case 11: matrizJogo[1][1] = cont % 2 == 0 ? "X" : "O"
+            case 12: matrizJogo[1][2] = cont % 2 == 0 ? "X" : "O"
+            case 20: matrizJogo[2][0] = cont % 2 == 0 ? "X" : "O"
+            case 21: matrizJogo[2][1] = cont % 2 == 0 ? "X" : "O"
+            case 22: matrizJogo[2][2] = cont % 2 == 0 ? "X" : "O"
+
+        default:
+            break
+        }
+
+        cont += 1
+        vencedor = cont > 4 ? winTie() : false
+        labelGanhador.text = (cont % 2 == 1) && vencedor == false ? "Player 'O' Turn": "Player 'X' Turn"
+        if (vencedor == true || cont == 9) {
+            endGame()
+        }
+        print (vencedor!)
+        print (matrizJogo)
+        print (cont)
+    }
+
+    func endGame () {
+        labelGanhador.text = (cont % 2 == 0) ? "Player 'O' Wins": "Player 'X' Wins"
+
+        if cont == 9 && vencedor == false {
+            labelGanhador.text = "Tie Game"
+        }
+        labelGanhador.isHidden = false
+        allButtons.forEach { button in
+            button.isEnabled = false
+        }
+        buttonPlayAgain.setTitle("Play Again", for: .normal)
+    }
+
+    @objc func playAgain() {
+        allButtons.forEach { button in
+            button.isEnabled = true
+            button.setTitle("", for: .normal)
+        }
+        labelGanhador.text = "Player 'X' Turn"
+        matrizJogo = matrizReset
+        cont = 0
+        buttonPlayAgain.setTitle("Restart", for: .normal)
+    }
+
+    func winTie() -> Bool{
+
+        for i in 0...2 {
+            if (matrizJogo[i][0] == matrizJogo[i][1]) && (matrizJogo[i][0] == matrizJogo[i][2]) && (matrizJogo[i][0] != "") {
+                return true
+            }
+        }
+        for i in 0...2 {
+            if (matrizJogo[0][i] == matrizJogo[1][i]) && (matrizJogo[0][i] == matrizJogo[2][i]) && (matrizJogo[0][i] != "") {
+                return true
+            }
+        }
+        if (matrizJogo[0][0] == matrizJogo[1][1]) && (matrizJogo[0][0] == matrizJogo[2][2]) && (matrizJogo[0][0] != "")  {
+            return true
+        }
+        if (matrizJogo[0][2] == matrizJogo[1][1]) && (matrizJogo[0][2] == matrizJogo[2][0]) && (matrizJogo[0][2] != "")  {
+            return true
+        }
+        return false
+    }
+
+    func addSubViewsModel () {
+        stackViewLinha.addArrangedSubview(stackViewColuna)
+        stackViewLinha.addArrangedSubview(stackViewColuna2)
+        stackViewLinha.addArrangedSubview(stackViewColuna3)
+        configureConstraintsButton()
+    }
+
     func configureConstraintsButton() {
         NSLayoutConstraint.activate([
-            button.heightAnchor.constraint(equalToConstant: 110),
-            button2.heightAnchor.constraint(equalToConstant: 110),
-            button3.heightAnchor.constraint(equalToConstant: 110),
-            button4.heightAnchor.constraint(equalToConstant: 110),
-            button5.heightAnchor.constraint(equalToConstant: 110),
-            button6.heightAnchor.constraint(equalToConstant: 110),
-            button7.heightAnchor.constraint(equalToConstant: 110),
-            button8.heightAnchor.constraint(equalToConstant: 110),
-            button9.heightAnchor.constraint(equalToConstant: 110),
+            button.heightAnchor.constraint(equalToConstant: 100),
+            button2.heightAnchor.constraint(equalToConstant: 100),
+            button3.heightAnchor.constraint(equalToConstant: 100),
+            button4.heightAnchor.constraint(equalToConstant: 100),
+            button5.heightAnchor.constraint(equalToConstant: 100),
+            button6.heightAnchor.constraint(equalToConstant: 100),
+            button7.heightAnchor.constraint(equalToConstant: 100),
+            button8.heightAnchor.constraint(equalToConstant: 100),
+            button9.heightAnchor.constraint(equalToConstant: 100),
     //            charactersButton.heightAnchor.constraint(equalToConstant: 52.0),
     //            locationsButton.heightAnchor.constraint(equalToConstant: 52.0),
         ])
